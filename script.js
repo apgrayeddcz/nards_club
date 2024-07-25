c1e08c4f8f110b43bbbeb4032cd0660993a17b96
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-25 16:20:52 +0300

    cc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function addBet(b_type, b_id) {}
 
 window.addEventListener('scroll', () => {
-    document.querySelector('._js').textContent = document.body.scrollTop
+    document.querySelector('._js').textContent = `${document.body.scrollTop} ${window.innerHeight}`
 }
 )
