105ecc795433eaa90dc4811bec8244954b3cfae3
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-25 16:37:11 +0300

    c

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function addBet(b_type, b_id) {}
 const INNER_HEIGHT = window.innerHeight;
 
 window.addEventListener('scroll', () => {
-  document.querySelector('._js').textContent = `${document.body.scrollTop} ${window.innerHeight}`;
+  document.querySelector('._js').textContent = `${37} ${document.body.scrollTop} ${window.innerHeight}`;
   if (window.innerHeight < INNER_HEIGHT) {
     window.innerHeight = INNER_HEIGHT;
   }
