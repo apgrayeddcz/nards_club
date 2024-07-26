856423f42bbe58ede107a44d1511e72f8ea05ebd
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-26 13:01:16 +0300

    cc

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function addBet(b_type, b_id) {}
 const INNER_HEIGHT = window.innerHeight;
 
 window.addEventListener('scroll', () => {
-  document.querySelector('._js').textContent = `${59} ${document.body.scrollTop} ${window.innerHeight}`;
+  document.querySelector('._js').textContent = `${01} ${document.body.scrollTop} ${window.innerHeight}`;
   if (window.innerHeight < INNER_HEIGHT) {
     document.querySelector('headers').style.top = `${INNER_HEIGHT - window.innerHeight}px`;
-    // document.querySelector('.panel').style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
+    document.querySelector('.panel').style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
   }
   else {
     document.querySelector('headers').style.top = 0;
-    // document.querySelector('.panel').style.bottom = 0;
+    document.querySelector('.panel').style.bottom = 0;
   }
 })
