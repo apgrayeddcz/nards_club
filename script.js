e4855a077c1e91a9d80c17a64a8068ae6ab7e086
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-26 12:51:08 +0300

    c

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ window.addEventListener('scroll', () => {
   document.querySelector('._js').textContent = `${37} ${document.body.scrollTop} ${window.innerHeight}`;
   if (window.innerHeight < INNER_HEIGHT) {
     document.querySelector('headers').style.top = `${INNER_HEIGHT - window.innerHeight}px`;
-    document.querySelector('.panel').style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
+    // document.querySelector('.panel').style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
   }
   else {
     document.querySelector('headers').style.top = 0;
-    document.querySelector('.panel').style.bottom = 0;
+    // document.querySelector('.panel').style.bottom = 0;
   }
 })
