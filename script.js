ed51dc560d520c2300fd7a4089334c685139adf3
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-30 14:47:40 +0300

    cc

--- script.js
+++ script.js
@@ -3,36 +3,35 @@ function changeBetSize(s_id) {}
 
 
 function checkListInStr(list, str) {
+  let status = false
   list.forEach(elem => {
     if (str.indexOf(elem) > 0) {
-      return true
+      status = true;
+      return
     }
   });
+  return status
 }
-// /iPhone|iPad|iPod/i.test(navigator.userAgent)
 
+if (checkListInStr(['iPhone','iPad','iPod'], navigator.userAgent)) {
+  const INNER_HEIGHT = window.innerHeight;
 
-window.addEventListener('onload', () => {
-  if (navigator.userAgent.indexOf('iPhone') < 0) {
-    const INNER_HEIGHT = window.innerHeight;
+  const headers = document.querySelector('headers');
+  const panel = document.querySelector('.panel');
+  const main = document.querySelector('main');
 
-    const headers = document.querySelector('headers');
-    const panel = document.querySelector('.panel');
-    const main = document.querySelector('main');
-
-    // panel.style.paddingBottom = `${6.66}vw`
-    window.addEventListener('scroll', () => {
-      document.querySelector('._js').textContent = `${32} ${document.body.scrollTop} ${window.innerHeight}`;
-      if (window.innerHeight < INNER_HEIGHT) {
-        headers.style.top = `${INNER_HEIGHT - window.innerHeight}px`;
-        panel.style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
-        main.style.paddingBottom = panel.style.height - (INNER_HEIGHT - window.innerHeight);
-      }
-      else {
-        headers.style.top = 0;
-        panel.style.bottom = 0;
-        main.style.paddingBottom = panel.style.height;
-      }
-    })
-  }
-})
+  panel.style.paddingBottom = `${6.66}vw`
+  window.addEventListener('scroll', () => {
+    document.querySelector('._js').textContent = `${47} ${document.body.scrollTop} ${window.innerHeight}`;
+    if (window.innerHeight < INNER_HEIGHT) {
+      headers.style.top = `${INNER_HEIGHT - window.innerHeight}px`;
+      panel.style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
+      main.style.paddingBottom = panel.style.height - (INNER_HEIGHT - window.innerHeight);
+    }
+    else {
+      headers.style.top = 0;
+      panel.style.bottom = 0;
+      main.style.paddingBottom = panel.style.height;
+    }
+  })
+}
