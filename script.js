192efca3fee5abe4dfb6295cdee13943b9a14da6
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-30 16:34:23 +0300

    ccc

--- script.js
+++ script.js
@@ -16,29 +16,28 @@ function convert_px_to_vw(px) {
   return 100 * px / window.innerWidth;
 }
 
+// Адаптация айфона под баг телеги
 if (checkListInStr(['iPhone','iPad','iPod'], navigator.userAgent)) {
   const INNER_HEIGHT = window.innerHeight;
-  const main_pd_b = 37.66
+  const MAIN_PD_B = 37.66
 
   const headers = document.querySelector('headers');
   const panel = document.querySelector('.panel');
   const main = document.querySelector('main');
 
   panel.style.paddingBottom = `${9.66}vw`;
-  main.style.paddingBottom = `${main_pd_b}vw`;
+  main.style.paddingBottom = `${MAIN_PD_B}vw`;
   
   window.addEventListener('scroll', () => {
     if (window.innerHeight < INNER_HEIGHT) {
       headers.style.top = `${INNER_HEIGHT - window.innerHeight}px`;
       panel.style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
-      main.style.paddingBottom = `${main_pd_b - convert_px_to_vw(INNER_HEIGHT - window.innerHeight)}vw`;
+      main.style.paddingBottom = `${MAIN_PD_B - convert_px_to_vw(INNER_HEIGHT - window.innerHeight)}vw`;
     }
     else {
       headers.style.top = 0;
       panel.style.bottom = 0;
-      main.style.paddingBottom = `${main_pd_b}vw`;
+      main.style.paddingBottom = `${MAIN_PD_B}vw`;
     }
-    document.querySelector('._js').textContent = `${50} ${document.body.scrollTop} ${window.innerHeight} ${main.style.paddingBottom}`;
-  
   })
 }
