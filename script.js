29cfe549fb4fcfa706d608a72a3f334d49fc2960
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-30 15:15:35 +0300

    cccc

--- script.js
+++ script.js
@@ -20,17 +20,19 @@ if (checkListInStr(['iPhone','iPad','iPod'], navigator.userAgent)) {
   const panel = document.querySelector('.panel');
   const main = document.querySelector('main');
 
-  panel.style.paddingBottom = `${9.66}vw`
+  panel.style.paddingBottom = `${9.66}vw`;
+  main.style.paddingBottom = `${37.66}vw`;
+  
   window.addEventListener('scroll', () => {
     if (window.innerHeight < INNER_HEIGHT) {
       headers.style.top = `${INNER_HEIGHT - window.innerHeight}px`;
       panel.style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
-      main.style.paddingBottom = panel.style.height - (INNER_HEIGHT - window.innerHeight);
+      // main.style.paddingBottom = panel.style.height - (INNER_HEIGHT - window.innerHeight);
     }
     else {
       headers.style.top = 0;
       panel.style.bottom = 0;
-      main.style.paddingBottom = panel.style.height;
+      // main.style.paddingBottom = panel.style.height;
     }
     document.querySelector('._js').textContent = `${47} ${document.body.scrollTop} ${window.innerHeight} ${main.style.paddingBottom}`;
   
