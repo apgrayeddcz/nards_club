c5f7e41fac63c2d3bd4f346640d7e4ec5548d220
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-07-30 15:18:39 +0300

    cccc

--- script.js
+++ script.js
@@ -15,24 +15,25 @@ function checkListInStr(list, str) {
 
 if (checkListInStr(['iPhone','iPad','iPod'], navigator.userAgent)) {
   const INNER_HEIGHT = window.innerHeight;
+  const main_pd_b = 37.66
 
   const headers = document.querySelector('headers');
   const panel = document.querySelector('.panel');
   const main = document.querySelector('main');
 
   panel.style.paddingBottom = `${9.66}vw`;
-  main.style.paddingBottom = `${37.66}vw`;
+  main.style.paddingBottom = `${main_pd_b}vw`;
   
   window.addEventListener('scroll', () => {
     if (window.innerHeight < INNER_HEIGHT) {
       headers.style.top = `${INNER_HEIGHT - window.innerHeight}px`;
       panel.style.bottom = `-${INNER_HEIGHT - window.innerHeight}px`;
-      // main.style.paddingBottom = panel.style.height - (INNER_HEIGHT - window.innerHeight);
+      main.style.paddingBottom = `${main_pd_b - (INNER_HEIGHT - window.innerHeight)}vw`;
     }
     else {
       headers.style.top = 0;
       panel.style.bottom = 0;
-      // main.style.paddingBottom = panel.style.height;
+      main.style.paddingBottom = `${main_pd_b}vw`;
     }
     document.querySelector('._js').textContent = `${47} ${document.body.scrollTop} ${window.innerHeight} ${main.style.paddingBottom}`;
   
