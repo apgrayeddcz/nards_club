7661b2668726acd90843eaeea8158195d2adb2ec
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2024-08-05 15:14:03 +0300

    ccc

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function addBet(b_type, b_id) {}
-function changeBetSize(s_id) {}
+let ACTIVE_BET_SIZE = 2
+const bet_size_list = [10, 50, 100, 500, 1000]
+let bet_list = {}
 
+function addBet(e) {
+  const i = JSON.parse(e.dataset.info.replaceAll("'", '"'));
+  const b_button = document.querySelector(`button.${i['type']}_item[data-info="${e.dataset.info}"]`)
+  const b_size = b_button.querySelector('.bet-size')
+
+
+  if (i['type'] in bet_list && i['id'] in bet_list[i['type']]) {
+    bet_list[i['type']][i['id']] += bet_size_list[ACTIVE_BET_SIZE];
+  }
+  else if (i['type'] in bet_list && (!(i['id'] in bet_list[i['type']]))) {
+    bet_list[i['type']][i['id']] = bet_size_list[ACTIVE_BET_SIZE];
+  }
+  else if (!(i['type'] in bet_list)) {
+    bet_list[i['type']] = {};
+    bet_list[i['type']][i['id']] = bet_size_list[ACTIVE_BET_SIZE];
+  }
+  document.querySelectorAll(`button.${i['type']}_item_active`).forEach(elem => elem.classList.remove(`.${i['type']}_item_active`));
+  b_button.classList.add(`.${i['type']}_item_active`);
+  b_size.style.display = 'flex'
+  b_size.querySelector('span').textContent = bet_list[i['type']][i['id']]
+}
+
+function changeBetSize(e) {
+  ACTIVE_BET_SIZE = Number(e.dataset.id);
+  document.querySelectorAll('.panel_bet-size_element_active').forEach(elem => elem.classList.remove('panel_bet-size_element_active'))
+  document.querySelector(`.panel_bet-size_element[data-id='${e.dataset.id}']`).classList.add('panel_bet-size_element_active')
+}
 
 function checkListInStr(list, str) {
   let status = false
@@ -41,3 +69,4 @@ if (checkListInStr(['iPhone','iPad','iPod'], navigator.userAgent)) {
     }
   })
 }
+changeBetSize(document.querySelector(`.panel_bet-size_element[data-id='${ACTIVE_BET_SIZE}']`))
