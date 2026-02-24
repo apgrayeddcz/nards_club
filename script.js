59b1090a032f7e9ba76a2bf1746d3925de767093
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2026-02-25 02:42:42 +0300

    55

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
 
-const user = tg.initDataUnsafe.user;
-const userId = user.id;
+// const user = tg.initDataUnsafe.user;
+// const userId = user.id;
 
-console.log("User ID:", userId);
+// console.log("User ID:", userId);
 
-document.querySelector("h1 div").textContent = String(window.location.search);
+params = window.location.search
+if (window.location.search == "") {
+  params = "Неизвестно"
+}
+document.querySelector("h1 div").textContent = params;
 // console.log(window.location.search)
 
 // const params = new URLSearchParams(window.location.search);
