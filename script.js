843ef8714ba3ea0d5f2bfd1384928c9971e86796
apgrayeddcz <117082894+apgrayeddcz@users.noreply.github.com>
2026-02-25 02:38:22 +0300

    2222

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-document.querySelector("h1 div").textContent = window.location.search
-console.log(window.location.search)
+const tg = window.Telegram.WebApp;
+
+const user = tg.initDataUnsafe.user;
+const userId = user.id;
+
+console.log("User ID:", userId);
+
+document.querySelector("h1 div").textContent = String(window.location.search);
+// console.log(window.location.search)
+
+// const params = new URLSearchParams(window.location.search);
+// const myToken = params.get("token");
+
+// document.querySelector("h1 div").textContent = String(window.location.search);
+// console.log(myToken);
